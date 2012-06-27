a0ad85c58ed3a0aa738b2e5fe5439e868aedead7
Ivan Malijkh <i.malijkh@gmail.com>
2012-06-27 16:48:05 +0200

    twitter id to_s

--- app/models/testimony.rb
+++ app/models/testimony.rb
@@ -10,7 +10,7 @@ class Testimony < ActiveRecord::Base
     Twitter.favorites('groningenrb', since_id: maximum(:tweet_id)).each do |tweet|
       unless exists?(tweet_id: tweet.id)
         create!(
-          tweet_id:     tweet.id,
+          tweet_id:     tweet.id.to_s,
           content:      tweet.text,
           screen_name:  tweet.user.screen_name
         )
