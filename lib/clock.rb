b01bcf7e3c45b1ce2d9b0888deccb559ee18aabd
Ivan Malijkh <i.malijkh@gmail.com>
2012-06-27 18:51:42 +0200

    test

--- lib/clock.rb
+++ lib/clock.rb
@@ -3,4 +3,8 @@ require File.expand_path('../../config/environment', __FILE__)
 require 'clockwork'
 include Clockwork
 
-every(1.minute, 'Pull Favorited Tweets', at: '00:00') { Testimony.pull_tweets }
+# every(1.minute, 'Pull Favorited Tweets', at: '00:00') { Testimony.pull_tweets }
+
+
+
+every(1.minute, 'Pull Favorited Tweets') { Testimony.pull_tweets }
