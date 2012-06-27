13b31316a0fe464f49211d3a54432a0d3c75e7b2
Ivan Malijkh <i.malijkh@gmail.com>
2012-06-27 18:51:06 +0200

    adding clock to pull tweets daily

---
+++ lib/clock.rb
@@ -0,0 +1,6 @@
+require File.expand_path('../../config/boot',        __FILE__)
+require File.expand_path('../../config/environment', __FILE__)
+require 'clockwork'
+include Clockwork
+
+every(1.minute, 'Pull Favorited Tweets', at: '00:00') { Testimony.pull_tweets }
