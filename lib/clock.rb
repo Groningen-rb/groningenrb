f4c57715c4e1e015e20b9286dc94482b6d4b4d62
Ivan Malijkh <i.malijkh@gmail.com>
2012-06-27 19:02:33 +0200

    change clock times

--- lib/clock.rb
+++ lib/clock.rb
@@ -3,8 +3,4 @@ require File.expand_path('../../config/environment', __FILE__)
 require 'clockwork'
 include Clockwork
 
-# every(1.minute, 'Pull Favorited Tweets', at: '00:00') { Testimony.pull_tweets }
-
-
-
-every(1.minute, 'Pull Favorited Tweets') { Testimony.pull_tweets }
+every(1.day, 'Pull Favorited Tweets', at: ['09:00', '15:00', '21:00']) { Testimony.pull_tweets }
